4fc5a2953ddff5014b0182251035429626014313
atish <atishbagate.aplswd@gmail.com>
2022-02-03 22:22:43 +0530

    config changed

--- config/database.php
+++ config/database.php
@@ -13,10 +13,11 @@ return [
     | Here you may specify which of the database connections below you wish
     | to use as your default connection for all database work. Of course
     | you may use many connections at once using the Database library.
+     // 'default' => env('DB_CONNECTION', 'mysql'),
     |
     */
 
-    // 'default' => env('DB_CONNECTION', 'mysql'),
+
     'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
@@ -67,7 +68,6 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
             'host' => $DATABASE_URL['host'],
             'port' => $DATABASE_URL['port'],
             'database' =>  ltrim($DATABASE_URL['path'], "/"),
